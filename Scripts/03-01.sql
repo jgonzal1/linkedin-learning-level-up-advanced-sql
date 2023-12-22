46245f9cc499bb3f6e8f4ba417a8dfe3e3da0068
Javier González Berenguel <jgb@aety.io>
2023-12-22 15:49:42 +0100

    + data model, ~ linting advanced select operations

--- Scripts/03-01.sql
+++ Scripts/03-01.sql
@@ -1,12 +1,12 @@
--- Summarise sales per year by using a CTE
-
-WITH cte AS (
-SELECT strftime('%Y', soldDate) AS soldYear, 
-  salesAmount
-FROM sales
+WITH common_table_expression AS (
+  SELECT strftime('%Y', soldDate) AS sold_year,
+    salesAmount as sales_amount
+  FROM sales
 )
-SELECT soldYear, 
-  FORMAT("$%.2f", sum(salesAmount)) AS AnnualSales
-FROM cte
-GROUP BY soldYear
-ORDER BY soldYear
+SELECT sold_year,
+  FORMAT('%,d', (SUM(sales_amount) / 100)) -- units
+  || FORMAT('.%02d $', SUM(sales_amount) % 100) AS annual_sales -- decimals
+  -- former: FORMAT("U%.2f $", SUM(sales_amount)) AS annual_sales
+FROM common_table_expression
+GROUP BY sold_year
+ORDER BY sold_year;
