395bf2bb0029edd0e6c7bf64ffeda08b595db957
Javier González Berenguel <jgb@aety.io>
2023-12-22 16:30:04 +0100

    ~ linting

--- Scripts/04-01.sql
+++ Scripts/04-01.sql
@@ -1,37 +1,38 @@
 -- For each sales person rank the car models they've sold most
-
 -- First join the tables to get the necessary data
-SELECT emp.firstName, emp.lastName, mdl.model, sls.salesId
+SELECT emp.firstName,
+  emp.lastName,
+  mdl.model,
+  sls.salesId
 FROM sales sls
-INNER JOIN employee emp
-  ON sls.employeeId = emp.employeeId
-INNER JOIN inventory inv
-  ON inv.inventoryId = sls.inventoryId
-INNER JOIN model mdl
-  ON mdl.modelId = inv.modelId
-
--- apply the grouping
-SELECT emp.firstName, emp.lastName, mdl.model,
+  INNER JOIN employee emp ON sls.employeeId = emp.employeeId
+  INNER JOIN inventory inv ON inv.inventoryId = sls.inventoryId
+  INNER JOIN model mdl ON mdl.modelId = inv.modelId;
+-- Apply the grouping
+SELECT emp.firstName,
+  emp.lastName,
+  mdl.model,
   count(model) AS NumberSold
 FROM sales sls
-INNER JOIN employee emp
-  ON sls.employeeId = emp.employeeId
-INNER JOIN inventory inv
-  ON inv.inventoryId = sls.inventoryId
-INNER JOIN model mdl
-  ON mdl.modelId = inv.modelId
-GROUP BY emp.firstName, emp.lastName, mdl.model
-
--- add in the windowing function
-SELECT emp.firstName, emp.lastName, mdl.model,
+  INNER JOIN employee emp ON sls.employeeId = emp.employeeId
+  INNER JOIN inventory inv ON inv.inventoryId = sls.inventoryId
+  INNER JOIN model mdl ON mdl.modelId = inv.modelId
+GROUP BY emp.firstName,
+  emp.lastName,
+  mdl.model;
+-- Add in the windowing function
+SELECT emp.firstName,
+  emp.lastName,
+  mdl.model,
   count(model) AS NumberSold,
-  rank() OVER (PARTITION BY sls.employeeId 
-              ORDER BY count(model) desc) AS Rank
+  rank() OVER (
+    PARTITION BY sls.employeeId
+    ORDER BY count(model) desc -- aggr alias not available unless new step
+  ) AS Rank -- by n models sold and by salesperson
 FROM sales sls
-INNER JOIN employee emp
-  ON sls.employeeId = emp.employeeId
-INNER JOIN inventory inv
-  ON inv.inventoryId = sls.inventoryId
-INNER JOIN model mdl
-  ON mdl.modelId = inv.modelId
-GROUP BY emp.firstName, emp.lastName, mdl.model
+  INNER JOIN employee emp ON sls.employeeId = emp.employeeId
+  INNER JOIN inventory inv ON inv.inventoryId = sls.inventoryId
+  INNER JOIN model mdl ON mdl.modelId = inv.modelId
+GROUP BY emp.firstName,
+  emp.lastName,
+  mdl.model;
--- Scripts/04-02.sql
+++ Scripts/04-02.sql
@@ -1,30 +1,37 @@
 -- Create a report showing sales per month and an annual total
-
--- get the needed data
-SELECT strftime('%Y', soldDate) AS soldYear, 
-  strftime('%m', soldDate) AS soldMonth, 
+-- Get the needed data
+SELECT strftime('%Y', soldDate) AS soldYear,
+  strftime('%m', soldDate) AS soldMonth,
   salesAmount
 FROM sales
-
--- apply the grouping
-SELECT strftime('%Y', soldDate) AS soldYear, 
+ORDER BY soldYear,
+  soldMonth;
+-- Apply the grouping
+SELECT strftime('%Y', soldDate) AS soldYear,
   strftime('%m', soldDate) AS soldMonth,
   SUM(salesAmount) AS salesAmount
 FROM sales
-GROUP BY soldYear, soldMonth
-ORDER BY soldYear, soldMonth
-
--- add the window function - simplify with cte
-with cte_sales as (
-SELECT strftime('%Y', soldDate) AS soldYear, 
-  strftime('%m', soldDate) AS soldMonth,
-  SUM(salesAmount) AS salesAmount
-FROM sales
-GROUP BY soldYear, soldMonth
+GROUP BY soldYear,
+  soldMonth
+ORDER BY soldYear,
+  soldMonth;
+-- Add the window function - simplify with cte
+WITH cte_sales AS (
+  SELECT strftime('%Y', soldDate) AS soldYear,
+    strftime('%m', soldDate) AS soldMonth,
+    SUM(salesAmount) AS salesAmount
+  FROM sales
+  GROUP BY soldYear,
+    soldMonth
 )
-SELECT soldYear, soldMonth, salesAmount,
+SELECT soldYear,
+  soldMonth,
+  salesAmount,
   SUM(salesAmount) OVER (
-    PARTITION BY soldYear 
-    ORDER BY soldYear, soldMonth) AS AnnualSales_RunningTotal
+    PARTITION BY soldYear
+    ORDER BY soldYear,
+      soldMonth
+  ) AS annualSalesRunningTotal
 FROM cte_sales
-ORDER BY soldYear, soldMonth
+ORDER BY soldYear,
+  soldMonth;
