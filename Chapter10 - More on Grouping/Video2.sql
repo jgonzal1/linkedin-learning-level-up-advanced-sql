07ffe9e4c49ad18f79bcea35f3e9d183b4d5a62a
Javier González Berenguel <jgb@aety.io>
2024-01-17 15:17:41 +0100

    # grouping and challenge linting

--- Chapter10 - More on Grouping/Video2.sql
+++ Chapter10 - More on Grouping/Video2.sql
@@ -1,3 +1,4 @@
+/*
 -- LinkedIn Learning 
 -- Advanced SQL - Query Processing Part 2 
 -- Ami Levin 2020 
@@ -7,8 +8,7 @@
 -- DBFiddle UK
 -- SQL Server https://dbfiddle.uk/?rdbms=sqlserver_2019&fiddle=04376649f89222ea605600103e5f01e4&hide=3
 -- PostgreSQL https://dbfiddle.uk/?rdbms=postgres_12&fiddle=58f53dadfba0b8ef1624a40776636155&hide=1
-
-/*   ______                       _             
+·    ______                       _             
 ·   / ____/________  __  ______  (_)___  ____ _ 
 ·  / / __/ ___/ __ \/ / / / __ \/ / __ \/ __ `/ 
 · / /_/ / /  / /_/ / /_/ / /_/ / / / / / /_/ /  
@@ -19,114 +19,194 @@
 ·            \__ \/ _ \/ __/ ___/               
 ·           ___/ /  __/ /_(__  )                
 ·          /____/\___/\__/____/               */
-
 -- Multi level aggregates
-SELECT  YEAR(Adoption_Date) AS Year,
-    MONTH(Adoption_Date) AS Month,
-    COUNT(*) AS Monthly_Adoptions
-FROM  Adoptions
-GROUP BY YEAR(Adoption_Date), MONTH(Adoption_Date);
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , MONTH (Adoption_Date) AS MONTH
+  , COUNT(*) AS Monthly_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    YEAR (Adoption_Date)
+  , MONTH (Adoption_Date)
+;
 
-SELECT  YEAR(Adoption_Date) AS Year,
-    COUNT(*) AS Annual_Adoptions
-FROM  Adoptions
-GROUP BY YEAR(Adoption_Date);
 
-SELECT  COUNT(*) AS Total_Adoptions
-FROM  Adoptions
-GROUP BY ();
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , COUNT(*) AS Annual_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    YEAR (Adoption_Date)
+;
+
+
+SELECT
+    COUNT(*) AS Total_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    ()
+;
+
 
 -- Add UNION ALL... no good
-SELECT  YEAR(Adoption_Date) AS Year,
-    MONTH(Adoption_Date) AS Month,
-    COUNT(*) AS Number_Of_Adoptions
-FROM  Adoptions
-GROUP BY YEAR(Adoption_Date), MONTH(Adoption_Date)
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , MONTH (Adoption_Date) AS MONTH
+  , COUNT(*) AS Number_Of_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    YEAR (Adoption_Date)
+  , MONTH (Adoption_Date)
 UNION ALL
-SELECT  YEAR(Adoption_Date) AS Year,
-    COUNT(*) AS Annual_Adoptions
-FROM  Adoptions
-GROUP BY YEAR(Adoption_Date)
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , COUNT(*) AS Annual_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    YEAR (Adoption_Date)
 UNION ALL
-SELECT  COUNT(*) AS Total_Adoptions
-FROM  Adoptions
-GROUP BY ();
+SELECT
+    COUNT(*) AS Total_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    ()
+;
+
 
 -- Try string placeholders... no good
-SELECT  YEAR(Adoption_Date) AS Year,
-    MONTH(Adoption_Date) AS Month,
-    COUNT(*) AS Number_Of_Adoptions
-FROM  Adoptions
-GROUP BY YEAR(Adoption_Date), MONTH(Adoption_Date)
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , MONTH (Adoption_Date) AS MONTH
+  , COUNT(*) AS Number_Of_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    YEAR (Adoption_Date)
+  , MONTH (Adoption_Date)
 UNION ALL
-SELECT  YEAR(Adoption_Date) AS Year,
-    'All Months' AS Month,
-    COUNT(*) AS Annual_Adoptions
-FROM  Adoptions
-GROUP BY YEAR(Adoption_Date)
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , 'All Months' AS MONTH
+  , COUNT(*) AS Annual_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    YEAR (Adoption_Date)
 UNION ALL
-SELECT  'All Years' AS Year,  
-    'All Months' AS Month,
-    COUNT(*) AS Total_Adoptions
-FROM  Adoptions
-GROUP BY ()
-ORDER BY Year, Month;
+SELECT
+    'All Years' AS YEAR
+  , 'All Months' AS MONTH
+  , COUNT(*) AS Total_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    ()
+ORDER BY
+    YEAR
+  , MONTH
+;
 
--- Use NULL placeholders... very good!
-SELECT  strftime('%Y', Adoption_Date) AS Year,
-    strftime('%m', Adoption_Date) AS Month,
-    COUNT(*) AS Monthly_Adoptions
-FROM  Adoptions
-GROUP BY 1,2;
 
-WITH Adoption_Aggrs AS (
-  SELECT strftime('%Y', Adoption_Date) AS Year,
-    strftime('%m', Adoption_Date) AS Month,
-    "Monthly Adoptions" AS Source,
-    COUNT(*) AS N_Adoptions
-FROM Adoptions
-GROUP BY 1,2,3
-)
-SELECT * from Adoption_Aggrs
+-- Usage of NULL placeholders... very good! SQLite-compatibility
+SELECT
+    strftime ('%Y', Adoption_Date) AS YEAR
+  , strftime ('%m', Adoption_Date) AS MONTH
+  , COUNT(*) AS Monthly_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    1
+  , 2
+;
+
+
+WITH
+    Adoption_Aggrs AS (
+        SELECT
+            strftime ('%Y', Adoption_Date) AS YEAR
+          , strftime ('%m', Adoption_Date) AS MONTH
+          , "Monthly Adoptions" AS Source
+          , COUNT(*) AS N_Adoptions
+        FROM
+            Adoptions
+        GROUP BY
+            1
+          , 2
+          , 3
+    )
+SELECT
+    *
+FROM
+    Adoption_Aggrs
 UNION ALL
-SELECT  Year,
-  NULL AS Month,
-  "Yearly adoptions" AS Source,
-  SUM(N_Adoptions) AS N_Adoptions
-FROM  Adoption_Aggrs
-GROUP BY 1,2,3
+SELECT
+    YEAR
+  , NULL AS MONTH
+  , "Yearly adoptions" AS Source
+  , SUM(N_Adoptions) AS N_Adoptions
+FROM
+    Adoption_Aggrs
+GROUP BY
+    1
+  , 2
+  , 3
 UNION ALL
-SELECT NULL AS Year,
-  NULL AS Month,
-  "Total adoptions" AS Source,
-  SUM(N_Adoptions) AS N_Adoptions
-FROM  Adoption_Aggrs
-GROUP BY 1,2,3;
+SELECT
+    NULL AS YEAR
+  , NULL AS MONTH
+  , "Total adoptions" AS Source
+  , SUM(N_Adoptions) AS N_Adoptions
+FROM
+    Adoption_Aggrs
+GROUP BY
+    1
+  , 2
+  , 3
+;
+
 
 -- Reuse lowest granularity aggregate in WITH clause
-WITH Aggregated_Adoptions
-AS
-(
-SELECT  YEAR(Adoption_Date) AS Year,
-    MONTH(Adoption_Date) AS Month,
-    COUNT(*) AS Monthly_Adoptions
-FROM  Adoptions
-GROUP BY YEAR(Adoption_Date), MONTH(Adoption_Date)
-)
-SELECT  *
-FROM  Aggregated_Adoptions
+WITH
+    Aggregated_Adoptions AS (
+        SELECT
+            YEAR (Adoption_Date) AS YEAR
+          , MONTH (Adoption_Date) AS MONTH
+          , COUNT(*) AS Monthly_Adoptions
+        FROM
+            Adoptions
+        GROUP BY
+            YEAR (Adoption_Date)
+          , MONTH (Adoption_Date)
+    )
+SELECT
+    *
+FROM
+    Aggregated_Adoptions
 UNION ALL
-SELECT  Year,
-    NULL,
-    COUNT(*)
-FROM  Aggregated_Adoptions
-GROUP BY Year
+SELECT
+    YEAR
+  , NULL
+  , COUNT(*)
+FROM
+    Aggregated_Adoptions
+GROUP BY
+    YEAR
 UNION ALL
-SELECT  NULL,
-    NULL,
-    COUNT(*)
-FROM  Aggregated_Adoptions
-GROUP BY ();
+SELECT
+    NULL
+  , NULL
+  , COUNT(*)
+FROM
+    Aggregated_Adoptions
+GROUP BY
+    ()
+;
 
 
 /* PostgreSQL
@@ -134,126 +214,169 @@ GROUP BY ();
 WITH Aggregated_Adoptions
 AS
 (
-SELECT  EXTRACT(year FROM Adoption_Date) AS Year,
-    EXTRACT(month FROM Adoption_Date) AS Month,
-    COUNT(*) AS Monthly_Adoptions
+SELECT  EXTRACT(year FROM Adoption_Date) AS Year
+  , EXTRACT(month FROM Adoption_Date) AS Month
+  , COUNT(*) AS Monthly_Adoptions
 FROM  Adoptions
 GROUP BY EXTRACT(year FROM Adoption_Date) , EXTRACT(month FROM Adoption_Date)
 )
 SELECT  *
 FROM  Aggregated_Adoptions
 UNION ALL
-SELECT  Year,
-    NULL,
-    COUNT(*)
+SELECT  Year
+  , NULL
+  , COUNT(*)
 FROM  Aggregated_Adoptions
 GROUP BY Year
 UNION ALL
-SELECT  NULL,
-    NULL,
-    COUNT(*)
+SELECT  NULL
+  , NULL
+  , COUNT(*)
 FROM  Aggregated_Adoptions
 GROUP BY ();
-*/
-
+ */
 -- GROUPING SETS
 -- Equivalent to no GROUP BY
-SELECT  COUNT(*) AS Total_Adoptions
-FROM  Adoptions
-GROUP BY GROUPING SETS  
-    (
-      ()
-    );
+SELECT
+    COUNT(*) AS Total_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    GROUPING SETS (())
+;
+
 
 -- Equivalent to GROUP BY YEAR(Adoption_Date)
-SELECT  YEAR(Adoption_Date) AS Year,
-    COUNT(*) AS Annual_Adoptions
-FROM  Adoptions
-GROUP BY GROUPING SETS  
-    (
-      YEAR(Adoption_Date)
-    )
-ORDER BY Year;
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , COUNT(*) AS Annual_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    GROUPING SETS (YEAR (Adoption_Date))
+ORDER BY
+    YEAR
+;
+
 
 -- Equivalent to GROUP BY YEAR(Adoption_Date), MONTH(Adoption_Date)
-SELECT  YEAR(Adoption_Date) AS Year,
-    MONTH(Adoption_Date) AS Month,
-    COUNT(*) AS Monthly_Adoptions
-FROM  Adoptions
-GROUP BY GROUPING SETS  
-    (
-      (
-        YEAR(Adoption_Date), MONTH(Adoption_Date)
-      )
-    )
-ORDER BY Year, Month;
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , MONTH (Adoption_Date) AS MONTH
+  , COUNT(*) AS Monthly_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    GROUPING SETS ((YEAR (Adoption_Date), MONTH (Adoption_Date)))
+ORDER BY
+    YEAR
+  , MONTH
+;
+
 
 -- Be careful with the parentheses!
-SELECT  YEAR(Adoption_Date) AS Year,
-    MONTH(Adoption_Date) AS Month,
-    COUNT(*) AS Monthly_Adoptions
-FROM  Adoptions
-GROUP BY GROUPING SETS  
-    (
-      YEAR(Adoption_Date), MONTH(Adoption_Date)
-    )
-ORDER BY Year, Month;
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , MONTH (Adoption_Date) AS MONTH
+  , COUNT(*) AS Monthly_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    GROUPING SETS (YEAR (Adoption_Date), MONTH (Adoption_Date))
+ORDER BY
+    YEAR
+  , MONTH
+;
+
 
 -- All in one...
-SELECT  YEAR(Adoption_Date) AS Year,
-    MONTH(Adoption_Date) AS Month,
-    COUNT(*) AS Monthly_Adoptions
-FROM  Adoptions
-GROUP BY GROUPING SETS  
-    (
-      (YEAR(Adoption_Date), MONTH(Adoption_Date)),
-      YEAR(Adoption_Date),
-      ()
-    )
-ORDER BY Year, Month;
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , MONTH (Adoption_Date) AS MONTH
+  , COUNT(*) AS Monthly_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    GROUPING SETS ((YEAR (Adoption_Date), MONTH (Adoption_Date)), YEAR (Adoption_Date), ())
+ORDER BY
+    YEAR
+  , MONTH
+;
+
 
 -- PostgreSQL
 -- All in one...
-SELECT  EXTRACT(year FROM Adoption_Date) AS Year,
-    EXTRACT(month FROM Adoption_Date) AS Month,
-    COUNT(*) AS Monthly_Adoptions
-FROM  Adoptions
-GROUP BY GROUPING SETS  
-    (
-      (EXTRACT(year FROM Adoption_Date), extract(month FROM Adoption_Date)),
-      EXTRACT(year FROM Adoption_Date),
-      ()
+SELECT
+    EXTRACT(
+        YEAR
+        FROM
+            Adoption_Date
+    ) AS YEAR
+  , EXTRACT(
+        MONTH
+        FROM
+            Adoption_Date
+    ) AS MONTH
+  , COUNT(*) AS Monthly_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    GROUPING SETS (
+        (
+            EXTRACT(
+                YEAR
+                FROM
+                    Adoption_Date
+            )
+          , EXTRACT(
+                MONTH
+                FROM
+                    Adoption_Date
+            )
+        )
+      , EXTRACT(
+            YEAR
+            FROM
+                Adoption_Date
+        )
+      , ()
     )
-ORDER BY Year, Month;
+ORDER BY
+    YEAR
+  , MONTH
+;
+
 
 -- Non hierarchical grouping sets
-SELECT  YEAR(Adoption_Date) AS Year,
-    Adopter_Email,
-    COUNT(*) AS Annual_Adoptions
-FROM  Adoptions
-GROUP BY GROUPING SETS  
-    (
-      YEAR(Adoption_Date),
-      Adopter_Email
-    );
+SELECT
+    YEAR (Adoption_Date) AS YEAR
+  , Adopter_Email
+  , COUNT(*) AS Annual_Adoptions
+FROM
+    Adoptions
+GROUP BY
+    GROUPING SETS (YEAR (Adoption_Date), Adopter_Email)
+;
+
 
 -- Handling NULLs
-SELECT  COALESCE(Species, 'All') AS Species,
-    CASE 
-      WHEN GROUPING(Breed) = 1
-      THEN 'All'
-      ELSE Breed
-    END AS Breed,
-    GROUPING(Breed) AS Is_This_All_Breeds,
-    COUNT(*) AS Number_Of_Animals
-FROM  Animals
-GROUP BY GROUPING SETS 
-    (
-      Species,
-      Breed,
-      ()
-    )
-ORDER BY Species, Breed;
+SELECT
+    COALESCE(Species, 'All') AS Species
+  , CASE
+        WHEN GROUPING(Breed) = 1 THEN 'All'
+        ELSE Breed
+    END AS Breed
+  , GROUPING(Breed) AS Is_This_All_Breeds
+  , COUNT(*) AS Number_Of_Animals
+FROM
+    Animals
+GROUP BY
+    GROUPING SETS (Species, Breed, ())
+ORDER BY
+    Species
+  , Breed
+;
+
 
 /*
 ·   ________  ________   _______   ______ 
@@ -261,4 +384,4 @@ ORDER BY Species, Breed;
 ·   / / / /_/ / __/    / __/ /  |/ / / / /
 ·  / / / __  / /___   / /___/ /|  / /_/ / 
 · /_/ /_/ /_/_____/  /_____/_/ |_/_____/  
-*/
+ */
